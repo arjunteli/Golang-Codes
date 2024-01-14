81dc359b4e82d93a0c430845af64dfacb01bd224
ARJUN TELI <modiarjunsahu1161999@gmail.com>
2024-01-14 23:39:25 +0530

    minor changes

--- 04function/main.go
+++ 04function/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
+
+//const loginToken = "Login Value"
 
 func calc1(x, y int) (int, int) {
 	var out1 = x + y
@@ -13,10 +18,20 @@ func calc2(x, y int) (out1, out2 int) {
 	out2 = x - y
 	return
 }
+func printTable(num, i int) {
+	if i == 11 {
+		return
+	}
+	fmt.Println(strconv.Itoa(num) + " x " + strconv.Itoa(i) + " = " + strconv.Itoa(num*i))
+	printTable(num, i+1)
+}
 
 func main() {
-	a, b := 5, 4
-	result1, result2 := calc2(a, b)
+	// a, b := 5, 4
+	// result1, result2 := calc2(a, b)
 
-	fmt.Println(result1, result2)
+	// fmt.Println(result1, result2)
+
+	num := 2
+	printTable(num, 1)
 }
